e8d2340ed431689905234e1d66bcd1d0845bff75
OVlasiuk <oleksandr.vlasiuk@vanderbilt.edu>
2016-06-22 15:36:17 -0600

    Move repulsion into a subroutine 'repel'

--- node_dis.m
+++ node_dis.m
@@ -1,6 +1,7 @@
 dim = 3;
 oct = 2^dim;
-
+% 
+% vertices of the unit cube
 cube_vectors = zeros(oct,3);
 count=1;
 for i=0:1
@@ -12,19 +13,14 @@ for i=0:1
     end
 end
 
-
-
 N = 15; %    number of boxes per side of the cube
-
-max_nodes_per_box = 20;  %00;
+max_nodes_per_box = 20;
 tolerance = 1e-4;
 
 
 corners = -ones(N^dim,dim);
 nodes = zeros(N^dim, max_nodes_per_box+1,dim); % the first element or vector will contain the number 
                     %  of nodes in the current box
-
-
                     
 tic
 
@@ -46,47 +42,27 @@ end
 
 num_nodes = sum(nodes(:,1,1));
 
-nodes_consecutive = zeros(num_nodes,dim);
+cnf = zeros(num_nodes,dim);
 count = 1;
 for i=1:N^dim
     cur_num = nodes(i,1,1);
-    nodes_consecutive(count:(count+cur_num-1),:) = nodes(i, 2:cur_num+1, :);
+    cnf(count:(count+cur_num-1),:) = nodes(i, 2:cur_num+1, :);
     count = count + cur_num;
 end
-toc
+toc 
+% % % % % % % % % % % % % 
 
+pbaspect([1 1 1])
 figure(1);
 fprintf( 'Number of nodes:      %d\n\n',  num_nodes )
-plot3(nodes_consecutive(:,1), nodes_consecutive(:,2), nodes_consecutive(:,3),  '.k');
+plot3(cnf(:,1), cnf(:,2), cnf(:,3),  '.k');
 
-
-
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
-% % % % %  REPULSION  % % % % % % % % % % % % % % % % % % % % % % % 
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
 
 k_value = 15;           % number of nearest neighbors used in the knnsearch
-repel_steps = 1;
+repel_steps = 5;
 
- cnf = nodes_consecutive;
+cnf = repel(cnf, k_value, repel_steps);
 
-IDX = knnsearch(cnf, cnf, 'k', k_value+1);
-forces = zeros(size(cnf,1), size(cnf,2));        
-
-for iter=1:repel_steps
-    for i=1:size(cnf,1)
-        force_i = normr(ones(k_value,1) * cnf(i,:) - cnf(IDX(i,2:end),:));
-%         
-        factor = 1/(iter*(100 + density(cnf(i,:))));
-%         
-        forces(i, :) = factor * sum(force_i, 1);
-    end
-    cnf = cnf + forces;
-end
-
-
-
+pbaspect([1 1 1])
 figure(2);
-% repel(nodes_consecutive);
 plot3(cnf(:,1), cnf(:,2), cnf(:,3),  '.k');
-tmp = cnf - nodes_consecutive;
--- repel.m
+++ repel.m
@@ -1,21 +1,35 @@
-function cnf = repel(cnf)
+function cnf = repel(cnf, k_value, repel_steps)
 
-k_value = 15;           % number of nearest neighbors used in the knnsearch
-repel_steps = 1;
+% k_value = 15;           % number of nearest neighbors used in the knnsearch
+% repel_steps = 1;
+bins = 50;
 
-[IDX] = knnsearch(cnf, cnf, 'k', k_value+1);
-forces = zeros(size(cnf,1), size(cnf,2));        
+[IDX, D] = knnsearch(cnf, cnf, 'k', k_value+1);
+forces = zeros(size(cnf));        
 
+fprintf( 'Minimal separation before repulsion:      %f\n',  min(D(:,2)) )
+fprintf( 'Mean separation before repulsion:      %f\n',  mean(D(:,2)) )
+figure(3);
+histogram(D(:,2),50);
+hold on;
 
 % force_i = zeros(k_value, 3);              
-
 
 for iter=1:repel_steps
     for i=1:size(cnf,1)
         force_i = normr(ones(k_value,1) * cnf(i,:) - cnf(IDX(i,2:end),:));
-        factor = 1/(iter*(20 + density(cnf(i,:))));
+%         
+        factor = 1/(iter*(1600 + density(cnf(i,:))));
+%         
         forces(i, :) = factor * sum(force_i, 1);
     end
     cnf = cnf + forces;
-    forces(4000:4010,:)
+    cnf(cnf<0) =  -cnf(cnf<0);
+    cnf(cnf>1) =  2-cnf(cnf>1);
 end
+
+[~, D] = knnsearch(cnf, cnf, 'k', k_value+1);   
+
+fprintf( 'Minimal separation before repulsion:      %f\n',  min(D(:,2)) )
+fprintf( 'Mean separation after:      %f\n',  mean(D(:,2)) )
+histogram(D(:,2),50);
