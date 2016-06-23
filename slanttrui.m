83801ab9ed9565e68486a91ff83dd10ff9bec6b1
Tim Michaels <Tim Michaels>
2016-06-23 12:14:21 -0500

    Fix
    Two Truis!

--- slanttrui.m
+++ slanttrui.m
@@ -1,7 +1,7 @@
 function r=slanttrui(xyz)
 
-n = [1,1,1]/sqrt(3);
+n = [1,1,0]/sqrt(2);
 proj = (xyz-dot(xyz,n)*n)';
 S = null(n);
-
+proj = proj.*[sqrt(2); sqrt(2);1];
 r = trui(frac_part(S\proj)');
