7ec5b0a5bad0bcd9fb88a0ee8c1f776cb90dae42
Tim Michaels <Tim Michaels>
2016-06-23 15:12:36 -0500

    Adjust vector
    Normal vector = [1,1,1]

--- slanttrui.m
+++ slanttrui.m
@@ -1,7 +1,10 @@
 function r=slanttrui(xyz)
 
-n = [1,1,0]/sqrt(2);
+n = [1,1,1]/sqrt(3);
 proj = (xyz-dot(xyz,n)*n)';
 S = null(n);
-proj = proj.*[sqrt(2); sqrt(2);1];
+
+T = [S(1,1),S(1,1);S(2,1), S(3,1); S(3,1),S(2,1)]/[1/2,-1/2,;-1/2,-1/2,;0,1];
+proj = T*(proj - [0;1/2;-1/2]);
+
 r = trui(frac_part(S\proj)');
