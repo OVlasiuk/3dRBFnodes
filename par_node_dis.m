ba2573e8106d90befa7fb59e40ff5ac176eaac48
OVlasiuk <oleksandr.vlasiuk@vanderbilt.edu>
2016-09-07 16:50:59 -0500

    section formatting

--- par_node_dis.m
+++ par_node_dis.m
@@ -1,53 +1,57 @@
 % % % % % % % % % MAIN SCRIPT FOR NODE SETTING: PARALLEL CPUS % % % % % % %
-% % % % % % % % % % % % % PARAMETERS  % % % % % % % % % % % % % % % % % % %
-                                                                                       % TODO replace index counts and such with masks
-                                                                                       % TODO column-major adjustment
+
+%% % % % % % % % % % % % PARAMETERS  % % % % % % % % % % % % % % % % % % %
+% TODO replace index counts and such with masks
+% TODO column-major adjustment                                                                                       
+
 dim = 3;
 oct = 2^dim;
-N = 50;                                                                                % number of boxes per side of the cube
+N = 20;  % number of boxes per side of the cube
+delta = 1/N;
 max_nodes_per_box = 15;  
-k_value = 15;                                                                          % number of nearest neighbors used in the knnsearch
+k_value = 15;     % number of nearest neighbors used in the knnsearch 
 repel_steps = 1;
 repel_power = 5;
 r1 = sqrt(2);
 r2 = sqrt(5);
 density = @trui;
+threshold = .7;   % domain choice threshold (used for strictly positive density)
 
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+%% populate vertices of the unit cube 
 
 
-cube_vectors = zeros(dim, oct);                                                          % populate vertices of the unit cube       
+cube_vectors = zeros(dim, oct);                                                                
 for i=1:dim
     len = 2 ^ (dim-i);
     for j=0:2^i-1                       
         cube_vectors(i, j*len+1:(j+1)*len) = mod(j,2);
     end
 end
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+
+%% node data array and masks
 nodes = zeros(dim, max_nodes_per_box, N^dim); 
 node_indices = false(max_nodes_per_box, N^dim);                                          % logical-styled integer array to be used as mask
 box_indices = false (N^dim,1);                                                            % boxes on the boundary
-corners = -ones(dim,N^dim);
-                                                                                        
+corner = -ones(dim,1);
+           
 
-                                                
+
+
+%% main parfor                                                
 tic
 parfor i=1:N^dim
-    corners(:,i) = [rem((i-1), N)  floor(rem(i-1, N^2)/N)  floor((i-1)/N^2)]/N;
-%    floor(rem(i-1, N^2)/N)
-    % TODO: this is not dimension-independent
-                                                                                        %       also, it is quite nasty
-                                                                                        %       
-    eval_pts = num2cell(bsxfun(@plus, corners(:,i), cube_vectors/N),1);
+    corner = [rem((i-1), N);  floor(rem(i-1, N^2)/N);  floor((i-1)/N^2)]/N;               % TODO: this is not dimension-independent
+    eval_pts = num2cell(bsxfun(@plus, corner, cube_vectors/N),1);
     fun_values = cellfun(density, eval_pts);               
     current_num_nodes = min(max_nodes_per_box-ceil(max_nodes_per_box * mean(fun_values)), max_nodes_per_box);    
     box = zeros(dim, max_nodes_per_box);    
     for j=1:current_num_nodes
         box(:,j) = [j/current_num_nodes;  frac_part(r1*j);  frac_part(r2*j)]/N;     % TODO: can this be vectorized?
-        box(:,j) = box(:,j) + corners(:,i);
+        box(:,j) = box(:,j) + corner;
     end    
     nodes(:,:,i) = box;   
-    if max(fun_values)>=.7                                                               % this definitely can be vectorized easily
+    if max(fun_values)>=threshold                                                               % TODO: harde-coded condition
+                                                                                         %  this definitely can be vectorized easily
         box_indices(i) = true;        
     end
     temp = false(max_nodes_per_box,1);
@@ -57,16 +61,17 @@ parfor i=1:N^dim
 end
 toc
 
+%% remove nodes outside the density support
 bdry_nodes = nodes(:, :, box_indices);                                                  % coordinates of the nodes that belong to boundary boxes
 flat = num2cell(reshape(bdry_nodes, dim, []), 1);                                       % 
 f_vals = cellfun(density, flat);                                                        % values of the density function at those nodes
-bdry_removed_indices = reshape( f_vals >.7, max_nodes_per_box, []);                     % indices of nodes that have to be removed
+bdry_removed_indices = reshape( f_vals > threshold, max_nodes_per_box, []);                     % indices of nodes that have to be removed; hard-coded
 node_indices(:, box_indices) = node_indices(:, box_indices) .* ~bdry_removed_indices;   % indices in the global nodeset
 node_indices_flat = reshape(node_indices, 1, []);  
 nodes = reshape (nodes, dim, []);
 cnf = nodes(:, node_indices_flat);                                                           % after removing all nodes with zero density
                              
-% % % % % % % % % % % % % % 
+%% node stats
 fprintf( '\nNumber of nodes:      %d\n',  length(cnf))
 fprintf( 'Mean number of nodes per box:      %d\n', mean(sum(node_indices,1) ))
 fprintf( 'Max number of nodes per box:      %d\n', max(sum(node_indices,1) ))
@@ -79,12 +84,13 @@ fprintf('\n');
 % figure(2);
 % plot3(cnf(1,:), cnf(2,:), cnf(3,:),  '.k');
 
+%% repel and save nodes
 fprintf( 'Performing %d repel steps.\n',  repel_steps)
-cnf = repel(cnf', k_value, repel_steps, repel_power);
+cnf = repel(cnf, k_value, repel_steps, repel_power);
 toc 
 
 pbaspect([1 1 1])
 % view([1 1 0])
 figure(1);
-plot3(cnf(:,1), cnf(:,2), cnf(:,3),  '.k');
-save('slanttrui.mat', 'cnf')
+plot3(cnf(1,:), cnf(2,:), cnf(3,:),  '.k');
+% save('slanttrui.mat', 'cnf')
--- repel.m
+++ repel.m
@@ -1,10 +1,10 @@
 function cnf = repel(cnf, k_value, repel_steps,s)
 
 bins = 50;
-riesz_s = @(x)riesz(x,s);
+riesz_s = @(x)riesz(x,s+1);
 
 
-[IDX, D] = knnsearch(cnf, cnf, 'k', k_value+1);
+[IDX, D] = knnsearch(cnf', cnf', 'k', k_value+1);
 forces = zeros(size(cnf));        
 
 step = min(D(:,2));
@@ -18,18 +18,23 @@ h1.FaceColor = [0 0 0.9];        % blue
 hold on;
 % % % % % % % % % % % % % % % % % % % % 
 
+% cnf_full = zeros(dim, k, size(cnf,1) );
+% for i=1:size(cnf,1)
+%     cnf_full(:,:,i) = 
+% end
+
 for iter=1:repel_steps
-   parfor i=1:size(cnf,1)
-       directions = ones(k_value,1) * cnf(i,:) - cnf(IDX(i,2:end),:);      %this has k_value rows
-       force_coeffs = cellfun(riesz_s,  num2cell(directions,2));            %this has k_value rows  
-       forces(i, :) =  normr(force_coeffs' * normr(directions));         % 1 x k_value * k_value x 3
+   parfor i=1:size(cnf,2)
+       directions =   cnf(:,i)* ones(1,k_value) - cnf(:,IDX(i,2:end));      %this has k_value rows
+       force_coeffs = cellfun(riesz_s,  num2cell(directions,1));            %this has k_value rows  
+       forces(:,i) =  normc(directions * force_coeffs' );         % 1 x k_value * k_value x 3
    end
     cnf = cnf + forces*step/3/iter;
     cnf(cnf<0) =  -cnf(cnf<0);
     cnf(cnf>1) =  2-cnf(cnf>1);
 end
 
-[~, D] = knnsearch(cnf, cnf, 'k', k_value+1);   
+[~, D] = knnsearch(cnf', cnf', 'k', k_value+1);   
 fprintf( 'Minimal separation after:      %f\n',  min(D(:,2)) )
 fprintf( 'Mean separation after:      %f\n',  mean(D(:,2)) )
 
