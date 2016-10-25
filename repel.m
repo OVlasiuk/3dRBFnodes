b990a39b64e659111e824beb2aa4a984eb471389
OVlasiuk <oleksandr.vlasiuk@vanderbilt.edu>
2016-10-25 18:28:05 -0500

    Optimize repel.m

--- repel.m
+++ repel.m
@@ -3,14 +3,13 @@ function cnf = repel(cnf, k_value, repel_steps,s)
 bins = 50;
 riesz_s = @(x)riesz(x,s+1);
 dim = size(cnf,1);
-
+pt_num = size(cnf,2);
 [IDX, D] = knnsearch(cnf', cnf', 'k', k_value+1);
 forces = zeros(size(cnf));        
-IDXflat = reshape(IDX', 1, []);
+IDX = IDX(:,2:end)';                     % drop the trivial first column in IDX
 
-cnf_neighbors = cnf(:,IDXflat);
-cnf_neighbors = reshape(cnf_neighbors, dim, k_value+1, size(cnf,2));
-cnf_neighbors = cnf_neighbors(:,2:end,:);
+cnf_neighbors = cnf(:,IDX);
+cnf_neighbors = reshape(cnf_neighbors, dim, k_value, pt_num);
 
 step = min(D(:,2));
 fprintf( 'Minimal separation before repel steps:      %f\n', step  )
@@ -31,18 +30,16 @@ hold on;
 
 
 for iter=1:repel_steps
-   parfor i=1:size(cnf,2)      
-%        directions =   cnf(:,i)* ones(1,k_value) - cnf(:,IDX(i,2:end));
-       directions =   cnf(:,i)* ones(1,k_value) - cnf_neighbors(:,:,i);          %this has dim rows, k_value columns
+   parfor i=1:pt_num    
+       directions =   cnf(:,i) * ones(1,k_value) - cnf_neighbors(:,:,i);         %this has dim rows, k_value columns
        force_coeffs = cellfun(riesz_s,  num2cell(directions,1));                 %this has k_value rows  
        forces(:,i) =  normc(directions * force_coeffs' );                        % 1 x k_value * k_value x 3
    end
     cnf = cnf + forces*step/3/iter;
     cnf(cnf<0) =  -cnf(cnf<0);
     cnf(cnf>1) =  2-cnf(cnf>1);
-    cnf_neighbors = cnf(:,IDXflat);
-    cnf_neighbors = reshape(cnf_neighbors, dim, k_value+1, size(cnf,2));
-    cnf_neighbors = cnf_neighbors(:,2:end,:);
+    cnf_neighbors = cnf(:,IDX);
+    cnf_neighbors = reshape(cnf_neighbors, dim, k_value, pt_num);
 end
 
 [~, D] = knnsearch(cnf', cnf', 'k', k_value+1);   
