fee7b16248bb42c5bf084f6b7ae64a5b4a53abe3
OVlasiuk <oleksandr.vlasiuk@vanderbilt.edu>
2017-05-04 23:10:27 -0500

    Use curl

--- helpers/etopo1load.m
+++ helpers/etopo1load.m
@@ -1,15 +1,22 @@
 %ETOPO1LOAD
-% Downloads and extracts the ETOPO1 data from NOAA website.
+% Downloads and extracts the ETOPO1 data from NOAA website. Uses curl on
+% UNIX and built-in websave otherwise.
 %   See also IN_DOMAIN
 s_old = pwd;
 s = char(mfilename('fullpath'));
 cd(s(1:end-10))                         % cd to the mfile folder; 
                                         % The constant 12 depends on the
                                         % length of the filename.
-
+url = 'https://www.ngdc.noaa.gov/mgg/global/relief/ETOPO1/data/bedrock/cell_registered/binary/etopo1_bed_c_i2.zip';
+filename = '../Output/etopo1_bed_c_i2.zip';
 fprintf('Please wait while the ETOPO data is downloaded from\n');
-fprintf('https://www.ngdc.noaa.gov/mgg/global/relief/ETOPO1/data/bedrock\n');
-urlwrite('https://www.ngdc.noaa.gov/mgg/global/relief/ETOPO1/data/bedrock/cell_registered/binary/etopo1_bed_c_i2.zip','../Output/etopo1_bed_c_i2.zip')
+fprintf('https://www.ngdc.noaa.gov/mgg/global/relief/ETOPO:1/data/bedrock\n');
+if isunix
+    !curl  -o ../Output/etopo1_bed_c_i2.zip 'https://www.ngdc.noaa.gov/mgg/global/relief/ETOPO1/data/bedrock/cell_registered/binary/etopo1_bed_c_i2.zip'
+else
+    websave(filename,url)
+end
 unzip('../Output/etopo1_bed_c_i2.zip','../Output/')
-fprintf('\n... done.\n');
+fprintf('\n\n... done.\n');
+clc;
 cd(s_old)
