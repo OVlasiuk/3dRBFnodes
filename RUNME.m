699beaf935c590c5f93895a4960995fa96740036
OVlasiuk <oleksandr.vlasiuk@vanderbilt.edu>
2017-05-08 14:48:11 -0500

    Add fancy input matching

--- RUNME.m
+++ RUNME.m
@@ -1,3 +1,4 @@
+function RUNME
 %RUNME
 % A demo script showcasing node distribution using irrational lattices and 
 % Riesz energy minimization. Serves as a wrapper to the example routines in
@@ -5,29 +6,41 @@
 % 
 %   See also NODE_DIS, NODE_EARTH.
 
+s_old = pwd;
 s = char(mfilename('fullpath'));
 cd(s(1:end-5))
 addpath helpers/
 
 fprintf('The following node distribution examples are available:\n');
-D = dir;
+D = dir('*.m');
 names = cell(size(D));
 for i=1:numel(D)
     names{i} = D(i).name;  
 end
 chnames = strvcat(names{:});
 idx  = strmatch('node_', chnames);
+I = cell(size(idx));
 for i=1:numel(idx)
     fprintf('%d     %s\n', i, names{idx(i)});
+    I{i} = num2str( i );
 end
 fprintf('\nMost of them will accept some input values, e.g., radial density,\n')
 fprintf('point inclusion function, etc.\n')
-fprintf('Type *help name_of_the_example* while in Matlab prompt to see details\n'); 
+fprintf('Type "help name_of_the_example" while in Matlab prompt to see details\n'); 
 fprintf('and possible input values. Alternatively, type the number of a \n')
 fprintf('script you''d like to run now with the default parameters, or press\n');
-I=input('return to go back to Matlab prompt.\n');
-if (I > 0) & (I <= numel(idx))
-    run(names{idx(I)})
-else
+fprintf('Return to go back to Matlab prompt. Type "A" to list all m-files\n');
+fprintf('in the current project. For most of these, typing "help name_of_the_file\n');
+inp=input('will produce some additional info.\n','s');
+if isempty(I)
+	d(s_old)
     return
+else
+    switch inp
+        case 'A'
+            dir *.m;
+            dir helpers*/*.m;
+        case I
+            run( names{ idx(eval(inp)) } )
+    end
 end
